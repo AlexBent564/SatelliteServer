f01b996246f22c09071fa21a4e03419c90e539e7
MiguelQuinones <42755523+MiguelQuinones@users.noreply.github.com>
2020-04-17 13:32:56 -0700

    Updated Fibonacci.java

--- src/appserver/job/impl/Fibonacci.java
+++ src/appserver/job/impl/Fibonacci.java
@@ -1,1 +1,26 @@
 
+package appserver.job.impl;
+
+import appserver.job.Tool;
+
+/**
+ * Class [Fibonacci] Performs the Fibonacci function when called and returns
+ * the number in the Fibonacci series according to the parameter
+ * 
+ */
+public class Fibonacci implements Tool {
+    
+    @Override
+    public Object go( Object parameters ) {
+        // if specified parameter is 1 return second value of Fibonacci series
+        if( ( Integer )parameters == 1 )
+        {
+            return 1;
+        }
+        // else specified parameter is greater than 1
+        else 
+        {
+            return go( (( Integer )parameters - 1) + ( ( Integer )parameters - 2) );
+        }
+    }
+}
