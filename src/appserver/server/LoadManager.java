04af086bed52ec2bfc925bfc1d661d3ed903c1b4
Stavros <stav.triantis@gmail.com>
2020-04-18 16:30:32 -0700

    loader test

--- src/appserver/server/LoadManager.java
+++ src/appserver/server/LoadManager.java
@@ -26,8 +26,8 @@ public class LoadManager{
 
     public String nextSatellite() throws Exception {
         
-        int numberSatellites = satellites.length();
-        int index;
+        int numberSatellites = satellites.size();
+        int index = 0;
         
         synchronized (satellites) {
             // implement policy that returns the satellite name according to a round robin methodology
@@ -35,13 +35,25 @@ public class LoadManager{
             //loop through array list of sattelites (sattelitemanager has connectivity info)
             //until rach end of array list, if at end of array list restart to begin at beginning
             
+           
+                
+        while( true )
+        {
+            boolean done = true;
+        
             for( index = 0; index < numberSatellites; index++ )
             {
-                return satellites[index];
+                System.out.println( (String) satellites.get(index)); //test print
+                return (String) satellites.get(index);
             }
+            index = 0;
+            done = false;
+        }
+             // ... name of satellite who is supposed to take job
+
+            
         }
         //filler value 'null'; return something else
-        return null; // ... name of satellite who is supposed to take job
         
     }
 }
