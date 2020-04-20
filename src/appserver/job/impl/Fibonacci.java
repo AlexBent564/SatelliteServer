086771bb4a77acba6dbb053c9a7bceac148fbc8a
MiguelQuinones <42755523+MiguelQuinones@users.noreply.github.com>
2020-04-19 18:07:24 -0700

    Added files via upload

--- src/appserver/job/impl/Fibonacci.java
+++ src/appserver/job/impl/Fibonacci.java
@@ -13,17 +13,15 @@ public class Fibonacci implements Tool {
     @Override
     public Object go( Object parameters ) {
         
-        // parameter will never be 0 but this block of code is necessary for the recursion
+        // if parameter is 1, return second value of Fibonacci series
         if( ( Integer )parameters == 0 )
         {
             return 0;
         }
-        // if parameter is 1, return second value of Fibonacci series
         else if( ( Integer )parameters == 1 )
         {
             return 1;
         }
-        // else parameter is greater than 1, return corresponding value of Fibonacci series
         else
         {
             return ( Integer )go( ( Integer )parameters - 1 ) + ( Integer )go( ( Integer )parameters - 2 );
